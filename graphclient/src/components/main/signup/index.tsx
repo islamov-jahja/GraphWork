61e2e84fabc2c2ed1846952dadee95f47bf5c2e4
islamov-jahja <yahyaislamov15@mail.ru>
2020-09-14 09:57:33 +0300

    добавил регистрацию

--- graphclient/src/components/main/signup/index.tsx
+++ graphclient/src/components/main/signup/index.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../../index.css';
 
 import {Redirect} from "react-router-dom";
+import {postData} from "../../../functions/functions";
 
 export const Signup: React.FC = () => {
+    const [firstName, setFirstName] = useState<string>()
+    const [email, setEmail] = useState<string>()
+    const [password, setPassword] = useState<string>()
+
     if (localStorage.getItem('token') != null) {
         return (
             <Redirect from='/login' to='/'/>
         );
 	 }
 	 
-	 const handleReg = () => {
-
+	 const handleReg = async (event: any) => {
+        event.preventDefault()
+        await postData('http://GraphWork/GraphServer/user/signup', {
+            name: firstName,
+            email: email,
+            password: password
+        }, '')
 	 }
+
+    function changeFirstName(event: React.ChangeEvent<HTMLInputElement>) {
+        setFirstName(event.target.value)
+    }
+
+    function changeEmail(event: React.ChangeEvent<HTMLInputElement>) {
+        setEmail(event.target.value)
+    }
+
+    function changePassword(event: React.ChangeEvent<HTMLInputElement>) {
+        setPassword(event.target.value)
+    }
 
     return (
         <div>
@@ -20,19 +42,19 @@ export const Signup: React.FC = () => {
                 <form className="col s12">
                     <div className="row">
                         <div className="input-field col s6">
-                            <input placeholder="Placeholder" id="first_name" type="text" className="validate"/>
+                            <input value={firstName} onChange={changeFirstName} placeholder="Placeholder" id="firstName" type="text" className="validate"/>
                                 <label htmlFor="first_name">Имя</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="email" type="email" className="validate"/>
+                            <input value={email} onChange={changeEmail} id="email" type="email" className="validate"/>
                             <label htmlFor="email">Почта</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="password" type="password" className="validate"/>
+                            <input value={password} onChange={changePassword} id="password" type="password" className="validate"/>
                             <label htmlFor="password">Пароль</label>
                         </div>
                     </div>
